636822b23a0952dc45b11a8b0a234904ce6be320
Tomer Harari <tharari21@gmail.com>
2022-08-20 18:54:40 -0400

    commit tree 2ccfa4fd74f42dbcece641781d25700d6bf43252

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all 
+    bakeries.to_json
+  end
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+  get '/baked_goods' do
+    BakedGood.all.to_json
+  end
+  get '/baked_goods/by_price' do 
+    baked_goods = BakedGood.order(price: :desc)
+    baked_goods.to_json
+  end
+  get '/baked_goods/most_expensive' do
+    BakedGood.order(price: :desc).first.to_json
+
+  end
 
 end
